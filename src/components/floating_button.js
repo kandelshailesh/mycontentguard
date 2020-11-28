f570e1ceff04e236679fa7941ef4cabc072ff0f2
Shailesh <shaileshkandel123@gmail.com>
2020-11-29 02:19:32 +0545

    Floating button

--- src/components/floating_button.js
+++ src/components/floating_button.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function Floating_Button({ setsupport }) {
   return (
-    <button
-      onClick={() => setsupport(prev => !prev)}
-      className='floating_button'
-    >
-      <i className='fa fa-question-circle'></i>
-      <label>Support</label>
-    </button>
+    <div className='floating_button__main'>
+      <button
+        onClick={() => setsupport(prev => !prev)}
+        className='floating_button'
+      >
+        <i className='fa fa-question-circle'></i>
+        <label>Support</label>
+      </button>
+    </div>
   );
 }
