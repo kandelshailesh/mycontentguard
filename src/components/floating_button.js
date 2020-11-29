41f2c9883ff3f9a57c5c6929ae6c484a7c106ec9
Shailesh <shaileshkandel123@gmail.com>
2020-11-29 21:13:51 +0545

    Floating button

--- src/components/floating_button.js
+++ src/components/floating_button.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 export default function Floating_Button({ setsupport }) {
   return (
-<<<<<<< HEAD
-    <div className='floating_button'>
-=======
     <div className='floating_button__main'>
->>>>>>> master
       <button
         onClick={() => setsupport(prev => !prev)}
         className='floating_button'
